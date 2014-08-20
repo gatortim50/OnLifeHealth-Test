799b7c360549e2ec2357783f808cb414502f04b7
Timothy McKee <railshead@gmail.com>
2014-08-20 18:43:49 -0400

    add new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OnLife::Application.config.secret_key_base = '2a7a959a37b221740472eee6f5e241f6f565db7616e1572ea756826e80aa501ff583fd7bc84a8bec4ac67babdbac188741b947cc869f215eb1c7db2862459f1e'
